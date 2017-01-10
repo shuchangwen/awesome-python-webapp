6ba673190cccdfc48375a4bfbdbe20d99de8e1b8
shuchangwen <shuchangwen@126.com>
2017-01-10 20:03:19 +0800

    modify orm.py

--- www/transwarp/orm.py
+++ www/transwarp/orm.py
@@ -1,3 +1,127 @@
 #!/usr/bin/env python
 #_*_ coding=utf-8 _*_
 
+_triggers = frozenset(['pre_insert', 'pre_update', 'pre_delete'])
+
+def _gen_sql(table_name, mappings):
+    """
+    类 ==> 表时 生成创建表的sql
+    :param table_name:
+    :param mappings:
+    :return:
+    """
+    pk = None
+    sql = ['-- generating SQL for %s:' % table_name, 'create table `%s` (' %table_name]
+    for f in sorted(mappings.values(), lambda x, y: cmp(x._order, y._order)):
+        if not hasattr(f, 'ddl'):
+            raise StandardError('no ddl in field "%s".' % f)
+        ddl = f.ddl
+        nullable = f.nullable
+        if f.primary_key:
+            pk = f.name
+        sql.append('    `%s` %s,' % (f.name, ddl) if nullable else '    `%s` %s not null,' % (f.name, ddl))
+    sql.append('    primary key(`%s`)' % pk)
+    sql.append(');')
+    return '\n'.join(sql)
+
+class Field(object):
+    """
+    保存数据库中的表的 字段属性
+    """
+    _count = 0
+
+    def __init__(self, **kw):
+        self.name = kw.get('name', None)
+        self._default = kw.get('default', None)
+        self.primary_key = kw.get('primary_key', False)
+        self.nullable = kw.get('nullable', False)
+        self.updatable = kw.get('updatable', True)
+        self.insertable = kw.get('insertable', True)
+        self.ddl = kw.get('ddl', '')
+        self._order = Field._count
+        Field._count += 1
+
+    @property
+    def default(self):
+        """
+        利用getter实现的一个写保护的实例属性
+        :return:
+        """
+        d = self._default
+        return d() if callable(d) else d
+
+    def __str__(self):
+        """
+        返回实例对象的描述信息
+        :return:
+        """
+        s = ['<%s:%s, default(%s), ' % (self.__class__.__name__, self.name, self.ddl, self._default)]
+        self.nullable and s.append('N')
+        self.updatable and s.append('U')
+        self.insertable and s.append('I')
+        s.append('>')
+        return ''.join(s)
+
+class IntegerField(Field):
+    """
+    保存Integer类型字段的属性
+    """
+    def __init__(self, **kw):
+        if 'default' not in kw:
+            kw['defualt'] = 0
+        if 'ddl' not in kw:
+            kw['ddl'] = 'bigint'
+        super(IntegerField, self).__init__(**kw)
+
+class FloatField(Field):
+    """
+    保存Float类型字段的属性
+    """
+    def __int__(self, **kw):
+        if 'default' not in kw:
+            kw['default'] = 0.0
+        if 'ddl' not in kw:
+            kw['ddl'] = 'bool'
+        super(FloatField, self).__init__(**kw)
+
+class BooleanField(Field):
+    """
+    保存BooleanField类型字段的属性
+    """
+    def __init__(self, **kw):
+        if not 'default' in kw:
+            kw['default'] = False
+        if not 'ddl' in kw:
+            kw['ddl'] = 'bool'
+        super(BooleanField, self).__init__(**kw)
+
+class TextFiled(Field):
+    """
+    保存Text类型字段的属性
+    """
+    def __init__(self):
+        if 'default' not in kw:
+            kw['default'] = ''
+        if 'ddl' not in kw:
+            kw['ddl'] = 'text'
+        super(TextFiled, self).__init__(**kw)
+
+class BlobField(Field):
+    """
+    宝不能Blob类型字段的属性
+    """
+    def __init__(self, **kw):
+        if 'default' not in kw:
+            kw['default'] = ''
+        if 'ddl' not in kw:
+            kw['ddl'] = 'blob'
+        super(BlobField, self).__init__()
+
+class VersionField(Field):
+    """
+    保存Version类型字段的属性
+    """
+    def __init__(self, name=None):
+        super(VersionField, self).__init__(name=name, default=0, ddl='bigint')
+
+
