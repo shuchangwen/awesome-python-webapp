59df2798c431e9210afd21e8eb61707a9f668f29
shuchangwen <shuchangwen@126.com>
2017-01-10 14:57:27 +0800

    delete db1.py and add orm.py

--- www/transwarp/db.py
+++ www/transwarp/db.py
@@ -1,68 +1,30 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-"""
-设计db模块的原因：
-  1. 更简单的操作数据库
-      一次数据访问：   数据库连接 => 游标对象 => 执行SQL => 处理异常 => 清理资源。
-      db模块对这些过程进行封装，使得用户仅需关注SQL执行。
-  2. 数据安全
-      用户请求以多线程处理时，为了避免多线程下的数据共享引起的数据混乱，
-      需要将数据连接以ThreadLocal对象传入。
-设计db接口：
-  1.设计原则：
-      根据上层调用者设计简单易用的API接口
-  2. 调用接口
-      1. 初始化数据库连接信息
-          create_engine封装了如下功能:
-              1. 为数据库连接 准备需要的配置信息
-              2. 创建数据库连接(由生成的全局对象engine的 connect方法提供)
-          from transwarp import db
-          db.create_engine(user='root',
-                           password='password',
-                           database='test',
-                           host='127.0.0.1',
-                           port=3306)
-      2. 执行SQL DML
-          select 函数封装了如下功能:
-              1.支持一个数据库连接里执行多个SQL语句
-              2.支持链接的自动获取和释放
-          使用样例:
-              users = db.select('select * from user')
-              # users =>
-              # [
-              #     { "id": 1, "name": "Michael"},
-              #     { "id": 2, "name": "Bob"},
-              #     { "id": 3, "name": "Adam"}
-              # ]
-      3. 支持事物
-         transaction 函数封装了如下功能:
-             1. 事务也可以嵌套，内层事务会自动合并到外层事务中，这种事务模型足够满足99%的需求
-"""
-
+#_*_ coding:utf-8 _*_
+import functools
+import logging
+import threading
 import time
 import uuid
-import functools
-import threading
-import logging
-
 
 # global engine object:
 engine = None
 
-
 def next_id(t=None):
     """
-    生成一个唯一id   由 当前时间 + 随机数（由伪随机数得来）拼接得到
+    生成一个唯一id 由当前时间 + 随机数（由伪随机数得来）拼接得到
+    :param t:
+    :return:
     """
     if t is None:
         t = time.time()
     return '%015d%s000' % (int(t * 1000), uuid.uuid4().hex)
 
-
-def _profiling(start, sql=''):
+def _profiling(start, sql = ''):
     """
     用于剖析sql的执行时间
+    :param start:
+    :param sql:
+    :return:
     """
     t = time.time() - start
     if t > 0.1:
@@ -73,8 +35,15 @@ def _profiling(start, sql=''):
 
 def create_engine(user, password, database, host='127.0.0.1', port=3306, **kw):
     """
-    db模型的核心函数，用于连接数据库, 生成全局对象engine，
+    db模型的核心函数，用于连接数据库，生成全局对象engine,
     engine对象持有数据库连接
+    :param user:
+    :param password:
+    :param database:
+    :param host:
+    :param port:
+    :param kwargs:
+    :return:
     """
     import mysql.connector
     global engine
@@ -87,38 +56,22 @@ def create_engine(user, password, database, host='127.0.0.1', port=3306, **kw):
     params.update(kw)
     params['buffered'] = True
     engine = _Engine(lambda: mysql.connector.connect(**params))
+    #logging.info("Engine is None: %s" % (engine is None))
     # test connection...
     logging.info('Init mysql engine <%s> ok.' % hex(id(engine)))
-
 
 def connection():
     """
     db模块核心函数，用于获取一个数据库连接
-    通过_ConnectionCtx对 _db_ctx封装，使得惰性连接可以自动获取和释放，
-    也就是可以使用 with语法来处理数据库连接
-    _ConnectionCtx    实现with语法
-    ^
-    |
-    _db_ctx           _DbCtx实例
-    ^
-    |
-    _DbCtx            获取和释放惰性连接
-    ^
-    |
-    _LasyConnection   实现惰性连接
+    :return:
     """
     return _ConnectionCtx()
 
-
 def with_connection(func):
     """
-    设计一个装饰器 替换with语法，让代码更优雅
-    比如:
-        @with_connection
-        def foo(*args, **kw):
-            f1()
-            f2()
-            f3()
+    设计一个装饰器 替换with语法
+    :param func:
+    :return:
     """
     @functools.wraps(func)
     def _wrapper(*args, **kw):
@@ -126,33 +79,14 @@ def with_connection(func):
             return func(*args, **kw)
     return _wrapper
 
-
 def transaction():
     """
     db模块核心函数 用于实现事物功能
-    支持事物:
-        with db.transaction():
-            db.select('...')
-            db.update('...')
-            db.update('...')
-    支持事物嵌套:
-        with db.transaction():
-            transaction1
-            transaction2
-            ...
+    :return:
     """
     return _TransactionCtx()
 
-
 def with_transaction(func):
-    """
-    设计一个装饰器 替换with语法，让代码更优雅
-    比如:
-        @with_transaction
-        def do_in_transaction():
-
-    StandardError: will cause rollback...
-    """
     @functools.wraps(func)
     def _wrapper(*args, **kw):
         start = time.time()
@@ -161,11 +95,14 @@ def with_transaction(func):
         _profiling(start)
     return _wrapper
 
-
 @with_connection
 def _select(sql, first, *args):
     """
-    执行SQL，返回一个结果 或者多个结果组成的列表
+    执行SQL, 返回一个结果或者多个结果组成的列表
+    :param sql: 
+    :param first:
+    :param args:
+    :return:
     """
     global _db_ctx
     cursor = None
@@ -186,41 +123,43 @@ def _select(sql, first, *args):
         if cursor:
             cursor.close()
 
-
 def select_one(sql, *args):
     """
-    执行SQL 仅返回一个结果
-    如果没有结果 返回None
-    如果有1个结果，返回一个结果
-    如果有多个结果，返回第一个结果
-
+    执行sql 仅返回一个结果
+    :param sql:
+    :param args:
+    :return:
     """
     return _select(sql, True, *args)
 
-
 def select_int(sql, *args):
     """
-    执行一个sql 返回一个数值，
-    注意仅一个数值，如果返回多个数值将触发异常
-
+    执行一个sql, 返回一个数值
+    :param sql:
+    :param args:
+    :return:
     """
     d = _select(sql, True, *args)
     if len(d) != 1:
         raise MultiColumnsError('Expect only one column.')
     return d.values()[0]
 
-
 def select(sql, *args):
     """
-    执行sql 以列表形式返回结果
+    执行sql以列表形式返回结果
+    :param sql:
+    :param args:
+    :return:
     """
     return _select(sql, False, *args)
-
 
 @with_connection
 def _update(sql, *args):
     """
-    执行update 语句，返回update的行数
+    执行update语句，返回update的行数
+    :param sql:
+    :param args:
+    :return:
     """
     global _db_ctx
     cursor = None
@@ -231,7 +170,7 @@ def _update(sql, *args):
         cursor.execute(sql, args)
         r = cursor.rowcount
         if _db_ctx.transactions == 0:
-            # no transaction enviroment:
+            #no transaction environment:
             logging.info('auto commit')
             _db_ctx.connection.commit()
         return r
@@ -239,22 +178,25 @@ def _update(sql, *args):
         if cursor:
             cursor.close()
 
-
 def update(sql, *args):
     """
-    执行update 语句，返回update的行数
+    执行update语句，返回update的行数
+    :param sql:
+    :param args:
+    :return:
     """
     return _update(sql, *args)
 
-
 def insert(table, **kw):
     """
-    执行insert语句
+    执行insert 语句
+    :param table:
+    :param kwargs:
+    :return:
     """
     cols, args = zip(*kw.iteritems())
     sql = 'insert into `%s` (%s) values (%s)' % (table, ','.join(['`%s`' % col for col in cols]), ','.join(['?' for i in range(len(cols))]))
     return _update(sql, *args)
-
 
 class Dict(dict):
     """
@@ -275,26 +217,21 @@ class Dict(dict):
     def __setattr__(self, key, value):
         self[key] = value
 
-
 class DBError(Exception):
     pass
-
 
 class MultiColumnsError(DBError):
     pass
 
-
 class _Engine(object):
     """
     数据库引擎对象
-    用于保存 db模块的核心函数：create_engine 创建出来的数据库连接
+    用于保存db模块的核心函数: create_engine创建出来的数据库连接
     """
     def __init__(self, connect):
         self._connect = connect
-
     def connect(self):
         return self._connect()
-
 
 class _LasyConnection(object):
     """
@@ -337,13 +274,15 @@ class _DbCtx(threading.local):
 
     def is_init(self):
         """
-        返回一个布尔值，用于判断 此对象的初始化状态
+        返回一个布尔值，用于判断此对象的初始化状态
+        :return:
         """
         return self.connection is not None
 
     def init(self):
         """
         初始化连接的上下文对象，获得一个惰性连接对象
+        :return:
         """
         logging.info('open lazy connection...')
         self.connection = _LasyConnection()
@@ -352,24 +291,24 @@ class _DbCtx(threading.local):
     def cleanup(self):
         """
         清理连接对象，关闭连接
+        :return:
         """
         self.connection.cleanup()
         self.connection = None
 
     def cursor(self):
         """
-        获取cursor对象， 真正取得数据库连接
+        获取cursor对象，真正取得数据库连接
+        :return:
         """
         return self.connection.cursor()
 
-
-# thread-local db context:
+#thread-local db context:
 _db_ctx = _DbCtx()
-
 
 class _ConnectionCtx(object):
     """
-    因为_DbCtx实现了连接的 获取和释放，但是并没有实现连接
+     因为_DbCtx实现了连接的 获取和释放，但是并没有实现连接
     的自动获取和释放，_ConnectCtx在 _DbCtx基础上实现了该功能，
     因此可以对 _ConnectCtx 使用with 语法，比如：
     with connection():
@@ -380,6 +319,7 @@ class _ConnectionCtx(object):
     def __enter__(self):
         """
         获取一个惰性连接对象
+        :return:
         """
         global _db_ctx
         self.should_cleanup = False
@@ -388,7 +328,7 @@ class _ConnectionCtx(object):
             self.should_cleanup = True
         return self
 
-    def __exit__(self, exctype, excvalue, traceback):
+    def __exit__(self, exc_type, exc_val, exc_tb):
         """
         释放连接
         """
@@ -397,29 +337,29 @@ class _ConnectionCtx(object):
             _db_ctx.cleanup()
 
 
+
 class _TransactionCtx(object):
     """
-    事务嵌套比Connection嵌套复杂一点，因为事务嵌套需要计数，
+     事务嵌套比Connection嵌套复杂一点，因为事务嵌套需要计数，
     每遇到一层嵌套就+1，离开一层嵌套就-1，最后到0时提交事务
     """
-
     def __enter__(self):
         global _db_ctx
         self.should_close_conn = False
         if not _db_ctx.is_init():
-            # needs open a connection first:
+            #needs open a connection first:
             _db_ctx.init()
             self.should_close_conn = True
         _db_ctx.transactions += 1
         logging.info('begin transaction...' if _db_ctx.transactions == 1 else 'join current transaction...')
         return self
 
-    def __exit__(self, exctype, excvalue, traceback):
+    def __exit__(self, exc_type, exc_val, exc_tb):
         global _db_ctx
         _db_ctx.transactions -= 1
         try:
             if _db_ctx.transactions == 0:
-                if exctype is None:
+                if exc_type is None:
                     self.commit()
                 else:
                     self.rollback()
@@ -428,7 +368,7 @@ class _TransactionCtx(object):
                 _db_ctx.cleanup()
 
     def commit(self):
-        global _db_ctx
+        global  _db_ctx
         logging.info('commit transaction...')
         try:
             _db_ctx.connection.commit()
@@ -445,7 +385,6 @@ class _TransactionCtx(object):
         _db_ctx.connection.rollback()
         logging.info('rollback ok.')
 
-
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
     create_engine('www-data', 'www-data', 'test')
@@ -453,3 +392,15 @@ if __name__ == '__main__':
     update('create table user (id int primary key, name text, email text, passwd text, last_modified real)')
     import doctest
     doctest.testmod()
+
+
+
+
+
+
+
+
+
+
+
+
