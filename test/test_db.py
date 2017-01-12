1d2b58db3327871bdf1208995f6d15f5890cf9c7
shuchangwen <shuchangwen@126.com>
2017-01-12 19:53:00 +0800

    add files

---
+++ test/test_db.py
@@ -0,0 +1,24 @@
+# test_db.py
+#!/usr/bin/env python
+#coding = utf-8
+import sys
+sys.path.append("..")
+
+from www.models import User, Blog, Comment
+from www.transwarp.db import *
+
+create_engine(user='www-data', password='www-data', database='awesome')
+
+u = User(name='Test', email='test@example.com', password='123456', image='about:blank')
+u.insert()
+
+print 'new user id:', u.id
+
+u1 = User.find_first('where email=?', 'test@example.com')
+print 'find user\'s name:', u1.name
+
+u1.delete()
+
+u2 = User.find_first('where email=?', 'test@example.com')
+print 'find user:', u2
+
